db7bd667297b419dd77d39435fdadb3254cacd74
Joshua Towney <joshtowney@hotmail.com>
2022-01-25 14:49:10 +1100

    stats level 1 done

--- server.js
+++ server.js
@@ -39,6 +39,13 @@ app.get('/api/:owners', (req, res) => {
     })
 })
 
+app.get('/api/stations/stats' , (req,res) => {
+    let sql = 'SELECT owner, count(*) FROM petrolStations GROUP BY owner ORDER BY count DESC;';
+    pool.query(sql, (err, dbRes) => {
+        res.json(dbRes.rows)
+    })
+})
+
 app.listen(8080, () => {
     console.log("Listening on port 8080");
 })
