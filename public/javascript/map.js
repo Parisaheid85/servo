b3ad371be2fd6518b826a8629c8facaca27b7edc
Parisaheide <parisaheid85@gmail.com>
2022-01-25 17:40:14 +1100

    marker-pop up

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -5,8 +5,8 @@ function initMap() {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 13,
   });
-  map.addListener('dragend', addMarkers)
-  map.addListener('zoom_changed', addMarkers)
+  map.addListener("dragend", addMarkers);
+  map.addListener("zoom_changed", addMarkers);
 }
 
 function getVisibleMapCoordinates() {
@@ -19,21 +19,40 @@ function addMarkers(petrolStations) {
     topLat: mapBoundaries.getNorthEast().lat(),
     topLng: mapBoundaries.getNorthEast().lng(),
     botLat: mapBoundaries.getSouthWest().lat(),
-    botLng: mapBoundaries.getSouthWest().lng()
-  }
-  axios.get(`http://localhost:8080/api/stations/nearest?botLat=${coordinatesObject.botLat}&botLng=${coordinatesObject.botLng}&topLat=${coordinatesObject.topLat}&topLng=${coordinatesObject.topLng}`)
-    .then(res => {
-      results = res.data
-      console.log(results)
+    botLng: mapBoundaries.getSouthWest().lng(),
+  };
+  axios
+    .get(
+      `http://localhost:8080/api/stations/nearest?botLat=${coordinatesObject.botLat}&botLng=${coordinatesObject.botLng}&topLat=${coordinatesObject.topLat}&topLng=${coordinatesObject.topLng}`
+    )
+    .then((res) => {
+      results = res.data;
+      console.log(results);
       // Loop through the results array and place a marker for each set of coordinates.
       for (let i = 0; i < results.length; i++) {
-        const latLng = new google.maps.LatLng(results[i].latitude, results[i].longitude);
-        new google.maps.Marker({
+        const latLng = new google.maps.LatLng(
+          results[i].latitude,
+          results[i].longitude
+        );
+        const infowindow = new google.maps.InfoWindow({
+          content: `<h1>${results[i].owner} </h1> 
+          <h2> ${results[i].name}</h2>`,
+        });
+
+        const marker = new google.maps.Marker({
           position: latLng,
           map: map,
         });
+
+        marker.addListener("click", () => {
+          infowindow.open({
+            anchor: marker,
+            map,
+            shouldFocus: false,
+          });
+        });
       }
-    })
+    });
 }
 
 // Get map bound values from viewport on change (maybe radius from map centre)
