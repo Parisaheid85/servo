cd0ba52adc413c85f0003b169b92b2c18185c622
Carter Deacon <carter.deacon@gmail.com>
2022-01-25 14:11:12 +1000

    commit to fix git

--- models/servo.js
+++ models/servo.js
@@ -2,7 +2,7 @@ const getPetrolStations = (coordinatesObject) => {
     let sql = `SELECT * FROM petrolStations 
     WHERE (latitude BETWEEN ${coordinatesObject.topLat} AND ${coordinatesObject.botLat}) 
     AND (longitude BETWEEN ${coordinatesObject.botLng} AND ${coordinatesObject.topLng});`
-    console.log(sql);
+    console.log(sql)
 }
 
 module.exports = {
