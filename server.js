9bf934dd2b29bd8ce6097bc185a196f5d46229d7
Parisaheide <parisaheid85@gmail.com>
2022-01-25 13:51:45 +1100

    rebase-main

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const res = require("express/lib/response");
 const app = express();
-const _ = require('underscore');
+const _ = require("underscore");
 
-app.use(express.static('public'));
+const { Pool } = require("pg");
 
-app.get('/', (req, res) => {
-    
-})
+const pool = new Pool({
+  database: "servo",
+});
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {});
+
+app.get("/api/stations/all", (req, res) => {
+  let sql = "select * from petrolStations Limit 500;";
+
+  pool.query(sql, (err, dbRes) => {
+    let result = res.json(dbRes.rows);
+    console.log(result);
+  });
+});
 
 app.listen(8080, () => {
-    console.log("Listening on port 8080");
-})
+  console.log("Listening on port 8080");
+});
